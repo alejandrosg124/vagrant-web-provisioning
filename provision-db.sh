3319ab74bb1a23fc6d2af54ddbc9323d0ae26988
alejandrosg124 <alejo.solarte1024@gmail.com>
2025-11-04 00:43:41 -0500

    Añado provision de base de datos con postgre y datos de prueba

--- provision-db.sh
+++ provision-db.sh
@@ -1,1 +1,13 @@
-#debes completar este archivo con los comandos necesarios para provisionar la base de datos
+#!/usr/bin/env bash
+
+sudo apt-get update -y
+
+sudo apt install -y postgresql postgresql-contrib
+
+sudo systemctl enable postgresql
+sudo systemctl start postgresql
+
+sudo -u postgres psql -c "CREATE USER vagrant WITH PASSWORD 'vagrant';"
+sudo -u postgres psql -c "CREATE DATABASE ejemplo OWNER vagrant;"
+sudo -u postgres psql -d ejemplo -c "CREATE TABLE amigos(id SERIAL PRIMARY KEY, nombre VARCHAR(50));"
+sudo -u postgres psql -d ejemplo -c "INSERT INTO amigos(nombre) VALUES ('Jose'), ('Alejandro'), ('Santiago');"
